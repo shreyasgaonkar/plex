0f7724d8b34df0324af1f1657cc8dcb7d3694f28
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2022-08-10 00:07:39 -0700

    Re-create deleted directory
    - Fix f'string syntax
    - Replace all `'` with `"`

--- plex.py
+++ plex.py
@@ -3,15 +3,15 @@
 import pysftp
 import os
 
-server = os.environ.get('server')
-username = os.environ.get('username')
-password = os.environ.get('password')
+server = os.environ.get("server")
+username = os.environ.get("username")
+password = os.environ.get("password")
 
-DIRS_TO_WATCH = [('auto', 'tv_shows'), ('completed', 'movies'),
-                 ('tv_completed', 'tv_shows')]
+DIRS_TO_WATCH = [("auto", "tv_shows"), ("completed", "movies"),
+                 ("tv_completed", "tv_shows")]
 LOCATION = {
-    "tv_shows": '/data2/tv_shows',
-    "movies": '/data1/plex/movies'
+    "tv_shows": "/data2/tv_shows",
+    "movies": "/data1/plex/movies"
 }
 
 
@@ -40,9 +40,10 @@ with pysftp.Connection(server, username=username, password=password) as sftp:
 
         try:
             sftp.get_r('.', local_dir)
-            print("[Success] Copied dir: {remote_dir} to {local_dir}")
+            print(f"[Success] Copied dir: {remote_dir} to {local_dir}")
         except Exception as e:
             print(e)
 
         print(f"Removing all files under dir: {remote_dir}")
         print(rm(remote_dir))
+        print(sftp.mkdir(remote_dir))
