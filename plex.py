f12641f2ed91a569dba19586727c8462e828befe
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2022-08-20 15:41:39 -0700

    Remove individual directories instead of bulk delete
    - Frees up space on the remote server as soon as individual
      files/directories are copied over to local
    - Better visibility into which files are being processed and
      files/directories which have been copied and deleted
    - Disable host key requirement for pysftp API call

--- plex.py
+++ plex.py
@@ -3,6 +3,10 @@
 import pysftp
 import os
 
+# Disable host key requirement for pysftp for cronjobs
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
+
 server = os.environ.get("server")
 username = os.environ.get("username")
 password = os.environ.get("password")
@@ -17,18 +21,28 @@ LOCATION = {
 
 def rm(path):
     files = sftp.listdir(path)
-    print(files)
     for f in files:
         filepath = os.path.join(path, f)
         try:
             sftp.remove(filepath)
         except IOError:
             rm(filepath)
-
     sftp.rmdir(path)
 
 
-with pysftp.Connection(server, username=username, password=password) as sftp:
+def download_and_delete_dir(dir_name, local_dir):
+    print(f"Downloading dir: {dir_name}")
+
+    try:
+        sftp.get_r('.', local_dir)
+        print(f"[Success] Copied dir: {dir_name} to {local_dir}")
+        print(rm(dir_name))
+        print(f"[Success] Deleted dir: {dir_name}")
+    except Exception as e:
+        print(e)
+
+
+with pysftp.Connection(server, username=username, password=password, cnopts=cnopts) as sftp:
 
     for dir, content_type in DIRS_TO_WATCH:
 
@@ -36,14 +50,8 @@ with pysftp.Connection(server, username=username, password=password) as sftp:
         local_dir = f"{LOCATION[content_type]}"
 
         sftp.chdir(remote_dir)
-        print(f"Starting download from {remote_dir} to {local_dir}")
+        print(f"Listing dir: {remote_dir}")
+        all_dirs = sftp.listdir()
 
-        try:
-            sftp.get_r('.', local_dir)
-            print(f"[Success] Copied dir: {remote_dir} to {local_dir}")
-        except Exception as e:
-            print(e)
-
-        print(f"Removing all files under dir: {remote_dir}")
-        print(rm(remote_dir))
-        print(sftp.mkdir(remote_dir))
+        for single_dir in all_dirs:
+            download_and_delete_dir(single_dir, local_dir)
